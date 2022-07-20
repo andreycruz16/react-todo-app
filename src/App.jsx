d00d4be01205d03f5bbe825fcfbef04ac1510251
Mark Andrey <markandreydelacruz@ymail.com>
2022-07-20 22:25:19 +0800

    add initial styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
-import { useState } from 'react'
-import './App.scss'
+import { Box, Text, Center, AspectRatio } from '@chakra-ui/react';
+// import './App.scss'
 
 function App() {
-
   return (
     <div className="App">
-      <h1>Todo App ⚡️</h1>
+      <Box bg="#f1f5f9" h="100vh">
+        <Box p="4" bgColor="gray.200">
+          <Center>
+            <Text fontSize="3em" fontFamily={'Poppins'}>
+              Todo App ⚡️
+            </Text>
+          </Center>
+        </Box>
+        <Box p="2" bgColor="gray.300">
+          <Center>
+            <Text fontSize="2em" fontFamily={'Poppins'}>
+              List 📃
+            </Text>
+          </Center>
+        </Box>
+        {/* <Box>
+          <AspectRatio ratio={1}>
+            <Box bgColor="gray.300" />
+          </AspectRatio>
+        </Box> */}
+      </Box>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
