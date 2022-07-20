fead14a6e47fc800643f5b95bae614a78f720008
Mark Andrey <markandreydelacruz@ymail.com>
2022-07-21 01:29:17 +0800

    add more styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,60 @@
-import { Box, Text, Center, AspectRatio } from '@chakra-ui/react';
+import { Box, Text, Center, Flex, Spacer, Square, Circle, ButtonGroup, Button } from '@chakra-ui/react';
 // import './App.scss'
 
 function App() {
   return (
     <div className="App">
-      <Box bg="#f1f5f9" h="100vh">
-        <Box p="4" bgColor="gray.200">
-          <Center>
-            <Text fontSize="3em" fontFamily={'Poppins'}>
-              Todo App ⚡️
-            </Text>
-          </Center>
-        </Box>
-        <Box p="2" bgColor="gray.300">
-          <Center>
-            <Text fontSize="2em" fontFamily={'Poppins'}>
-              List 📃
-            </Text>
-          </Center>
-        </Box>
-        {/* <Box>
-          <AspectRatio ratio={1}>
-            <Box bgColor="gray.300" />
-          </AspectRatio>
-        </Box> */}
+      <Box bg="#f1f5f9">
+        <Center>
+          <Box bg="#f1f5f9" h="100vh" w="740px">
+            <Box p="4" bgColor="gray.300">
+              <Flex>
+                <Text fontSize="2em" fontFamily={'Poppins'} fontWeight={'700'} color={'#0f172a'}>
+                  todo
+                </Text>
+                <Text fontSize="2em" fontFamily={'Poppins'} fontWeight={'300'} color={'#94a3b8'}>
+                  .app 📓
+                </Text>
+                <Spacer />
+                <Box display="flex" alignItems="center">
+                  <Button
+                    onClick={() => {
+                      alert('open modal');
+                    }}
+                  >
+                    <Text fontSize="1.5em" fontFamily={'Poppins'} fontWeight={'700'} color={'#94a3b8'}>
+                      ?
+                    </Text>
+                  </Button>
+                </Box>
+              </Flex>
+            </Box>
+
+            <Box p="4">
+              <Flex>
+                <Text fontSize="1em" fontFamily={'Poppins'}>
+                  The simplest way to make a todo lists ⚡
+                </Text>
+              </Flex>
+            </Box>
+
+            <Box p="4">
+              <Flex>
+                <Text fontSize="1.5em" fontFamily={'Poppins'}>
+                  List 📃
+                </Text>
+              </Flex>
+            </Box>
+
+            <Box p="4" bgColor="gray.100">
+              <Flex>
+                <Text fontSize="1.5em" fontFamily={'Poppins'}>
+                  Add New ➕
+                </Text>
+              </Flex>
+            </Box>
+          </Box>
+        </Center>
       </Box>
     </div>
   );
